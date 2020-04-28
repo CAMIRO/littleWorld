dfd1c55982192d3256fe4ba9a1135e3f8126be14
Camilo <juan@codegiant.io>
2020-04-28 10:06:16 +0800

    adding apollo.js, react-router and styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,64 @@
 import React from 'react';
-import  { Test } from './components/test'
-
+import styled from 'styled-components'
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 const App: React.FC = () => {
+  
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
   return (
-  <div>
-      little World
-      <Test 
-        text='hello'
-        person={{ firstName: "", lastName: "" }}
-        handleChange={() => console.log('from App')}
-      />
-      
-  </div> 
-    
+     <Wrapper>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  overflow: hidden;
+  width: 100%;
+  min-height: 100%;
+  flex-direction: row;
+`
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://localhost:5000/graphql',
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
