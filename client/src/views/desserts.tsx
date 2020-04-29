af8d7101b7777ee68e43c48db8438e7b5e7885fb
Camilo <juan@codegiant.io>
2020-04-29 09:32:57 +0800

    adding bootsrap and changing the font

--- client/src/views/desserts.tsx
+++ client/src/views/desserts.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from 'react-bootstrap'
+import { Carousel, Button } from 'react-bootstrap'
 
 interface DessertsProps {
 
 }
 
-export const Desserts: React.FC<DessertsProps> = ({}) => {
+export const Desserts: React.FC<DessertsProps> = ({ }) => {
     return (
         <Wrapper>
             <Content>
-                <Button>hallo</Button>
+                <Carousel>
+                    <Carousel.Item>
+                        <Button>hello</Button>
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Button>hello2</Button>
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Button>hello3</Button>
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
                 <h2>Desserts :</h2>
             </Content>
         </Wrapper>
@@ -19,7 +41,7 @@ export const Desserts: React.FC<DessertsProps> = ({}) => {
 // TODO: Adjust the height in Wrapper div
 const Wrapper = styled.div`
   padding: 20px;
-  height: 3000px;
+  width: inherit;
   overflow-y: scroll;
 `
 const Content = styled.div`
