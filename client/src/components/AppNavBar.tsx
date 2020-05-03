20a15c3dcd00d1f5f596f7080d1075c9ea0ca10a
camilo <juan@codegiant.io>
2020-05-03 13:25:10 +0800

    starting adding the nav boostrap styles

--- client/src/components/AppNavBar.tsx
+++ client/src/components/AppNavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from "react-router-dom";
-
+import { Nav } from 'react-bootstrap';
 import { Text } from "../containers/language";
 
 export const AppNavBar: React.FC = () => {
@@ -14,28 +14,35 @@ export const AppNavBar: React.FC = () => {
   return (
     <Wrapper>
       <MenuContainer>
-        <Logo src="/logo.png" alt="Logo" />
-        <nav>
-          <ul>
-            <MenuItem>
-              <li onClick={isSelected}>
-                <Link to="/">
-                  <Text tid="exploreHeader" />
-                </Link>
-              </li>
-            </MenuItem>
-            <MenuItem>
-              <li onClick={isSelected}>
-                <Link to="/fastFood">Fast Food</Link>
-              </li>
-            </MenuItem>
-            <MenuItem>
-              <li onClick={isSelected}>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </MenuItem>
-          </ul>
-        </nav>
+
+        <Nav fill>
+          <Nav.Item>
+            <Nav.Link onClick={isSelected}>
+              <Link to="/">
+                <Logo src="/logo.png" alt="Logo" />
+              </Link>
+            </Nav.Link>
+
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={isSelected}>
+              <Link to="/">
+                <Text tid="exploreHeader" />
+              </Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={isSelected}>
+              <Link to="/fastFood">Fast Food</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={isSelected}>
+              <Link to="/contact">Contact</Link>
+            </Nav.Link>
+          </Nav.Item>
+
+        </Nav>
       </MenuContainer>
     </Wrapper>
 
@@ -43,7 +50,7 @@ export const AppNavBar: React.FC = () => {
 }
 const Wrapper = styled.div`
   width: 96px;
-  min-width: 96px;
+  min-width: 98px;
   z-index: 102;
   box-shadow: inset -7px 0px 22px -7px rgba(0, 0, 0, 0.18);
   border-right: 1px solid #e4e4e4;
@@ -55,19 +62,10 @@ const Logo = styled.img`
   height: 68px;
 `
 const MenuContainer = styled.div`
-  min-height: 66px;
+  min-height: 70px;
   position: fixed;
   width: inherit;
   display: flex;
   flex-direction: column;
   align-items: center;
 `
-const MenuItem = styled.div`
-  height: 54px;
-  width: 100%;
-  text-align: center;
-  align-items: center;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  `
