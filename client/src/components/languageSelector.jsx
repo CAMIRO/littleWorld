680eddea2bc535661ca1206093a712b2d88de94d
CAMIRO <juancamilosc@hotmail.com>
2020-05-06 12:32:26 +0800

    adding boostrap styles to selector

--- client/src/components/languageSelector.jsx
+++ client/src/components/languageSelector.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-
 import { languageOptions } from "../languages";
-
 import { LanguageContext } from "../containers/language";
+import styled from "styled-components";
+import { Form, Row, Col } from 'react-bootstrap'
 
-export default function LanguageSelector() {
+const LanguageSelector = () => {
   const languageContext = useContext(LanguageContext);
 
   const handleLanguageChange = (event) => {
@@ -16,12 +16,27 @@ export default function LanguageSelector() {
   };
 
   return (
-    <select onChange={handleLanguageChange} value={languageContext.language.id}>
-      {languageOptions.map((item) => (
-        <option key={item.id} value={item.id}>
-          {item.text}
-        </option>
-      ))}
-    </select>
+    <Wrapper>
+      <Form.Row>
+        <Col md={{ span: 2, offset: 9 }}>
+          <Form.Group>
+            <Form.Control as={Col} size="sm" as="select" onChange={handleLanguageChange} value={languageContext.language.id}>
+              {languageOptions.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.text}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Col>
+      </Form.Row>
+    </Wrapper>
   );
 }
+export default LanguageSelector
+
+const Wrapper = styled.div`
+ min-height: 30px;
+ padding: 20px 0;
+`;
+
