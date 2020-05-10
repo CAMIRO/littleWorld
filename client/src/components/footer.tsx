e029ba5cf8ae17e3a601cf6fd57771a55aab87a6
CAMIRO <juancamilosc@hotmail.com>
2020-05-10 14:50:43 +0800

    adding boostrap grid to footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,37 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { Container, Row, Col } from 'react-bootstrap'
 export const Footer: React.FC = () => {
 
     const date = new Date();
     const currentYear = date.getFullYear();
     return (
-        <Wrapper>
-            <Item>
-                ItemOne
-            </Item>
-            <Credits>
-                Little World - {currentYear}
-            </Credits>
-        </Wrapper>
+        <MyContainer fluid>
+            <MyRow className="justify-content-md-center align-items-center" >
+                <MyCol sm={8}>For custom order requests, questions, or just to get in touch please email <Atag href="mailto: littleworld0326@gmail.com">littleworld0326@gmail.com</Atag></MyCol>
+
+                <MyCol sm={4}>row 1 - col 2</MyCol>
+            </MyRow>
+            <MyRow className="justify-content-md-center" >
+                <Credits>Little World - {currentYear}</Credits>
+            </MyRow>
+        </MyContainer>
     )
 }
 
-const Wrapper = styled.div`
-flex: 1;
+const MyContainer = styled(Container)`
 background-color: #68c3c8;
-min-height: -webkit-fill-available;
 `
 
-const Item = styled.div`
-flex-grow: 1;
+const MyRow = styled(Row)`
 border: 1px solid blue;
-min-height: 60px;
+font-size: 0.8rem;
+&:first-child  {
+    min-height: 68px; 
+}
 `
-
-const Credits = styled.div`
-flex-grow: 3;
-text-align: center;
-font-size: 13px;
-
+const MyCol = styled(Col)`
+    text-align: left;
+    padding-left: 40px;
+    
+`
+const Credits = styled(Col)`
+    text-align: center;
+    font-size: 0.7rem;
+    margin-bottom: 5px;
+`
+const Atag = styled.a`
+color: #EC7637;
+&:hover {
+text-decoration: none;
+color: #FFFFFF;    
+}
 `
