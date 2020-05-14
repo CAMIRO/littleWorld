0c8ff695c4da3b1f1ccd3af8ecd880fbaa14f2af
CAMIRO <juancamilosc@hotmail.com>
2020-05-14 17:26:21 +0800

    now we have some Cards

--- client/src/views/desserts.tsx
+++ client/src/views/desserts.tsx
@@ -11,42 +11,150 @@ export const Desserts: React.FC<DessertsProps> = ({ }) => {
       <Content>
         <Container fluid >
           <Row >
-            <MyCol xl={4} sm={4}>Column 1</MyCol>
-            <MyCol xl={4} sm={4}>Column 2</MyCol>
-            <MyCol xl={4} sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 1</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
-            <MyCol sm={4}>Column 2</MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
+            <MyCol sm={4}>
+              <Card style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </MyCol>
           </Row>
         </Container>
       </Content>
@@ -60,14 +168,13 @@ const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
-  padding: 5px;
 `;
 const Content = styled.div`
   width: 100%;
   overflow: auto;
-  padding: 0 15px 100px;
+  padding-bottom: 100px;
 `;
 const MyCol = styled(Col)`
-   border: 1px solid red;
+margin-top: 30px;
    
 `
