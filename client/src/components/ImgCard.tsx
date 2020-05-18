71ce3661b3193dc0ddd3d74e67ed17ea13fce8e2
CAMIRO <juancamilosc@hotmail.com>
2020-05-18 11:17:30 +0800

    adding a simple hover effect on ImgCard component

--- client/src/components/ImgCard.tsx
+++ client/src/components/ImgCard.tsx
@@ -9,17 +9,24 @@ export const ImageCard: React.FC<ImageCardProps> = ({ cardImgLink, cardTitle }) 
 
     return (
         <MyCol sm={3}>
-            <Card bg="primary" className="text-white" onClick={() => console.log('heey')} style={{ width: '100%' }}>
+            <MyCard bg="primary" className="text-white" onClick={() => console.log('heey')} style={{ width: '100%' }}>
                 <Card.Img variant="top" src={`${cardImgLink} cap`} />
                 <Card.ImgOverlay className="column justify-content-end">
                     <Card.Title >{cardTitle}</Card.Title>
                 </Card.ImgOverlay>
 
-            </Card>
+            </MyCard>
         </MyCol>
     )
 
 }
 const MyCol = styled(Col)`
-margin-top: 30px;
+    margin-top: 30px;
+`
+const MyCard = styled(Card)`
+    :hover{
+     cursor: pointer;   
+     box-shadow: 0 5px 14px rgba(0, 0, 0, .2);
+    }
+
 `
