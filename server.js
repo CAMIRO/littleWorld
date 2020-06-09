9a313c086cbe5310d0faeba4d34b0b12482df6eb
CAMIRO <juancamilosc@hotmail.com>
2020-06-09 19:51:14 +0800

    second try

--- server.js
+++ server.js
@@ -31,12 +31,17 @@ app.use('/graphql', graphqlHTTP({
 //     res.sendFile('index.html', { root });
 // })
 
-
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+
+// app.use(express.static(path.join(__dirname, "public")))
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// });
 
 const PORT = process.env.PORT || 5000;
 
