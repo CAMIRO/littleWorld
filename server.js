02aa4702c910211eb021cf2f38a4a25c51c6bf14
CAMIRO <juancamilosc@hotmail.com>
2020-06-09 20:00:51 +0800

    changing proxy

--- server.js
+++ server.js
@@ -17,31 +17,12 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-
-// static folder
-// app.use(express.static('public'));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// })
-
-//const root = require('path').join(__dirname, 'public')
-// app.use(express.static(path.join(__dirname, "public")));
-// app.get('*', (req, res) => {
-//     res.sendFile('index.html', { root });
-// })
-
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-// app.use(express.static(path.join(__dirname, "public")))
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// });
 
 const PORT = process.env.PORT || 5000;
 
